3a128fe78b30b959db60850a4ce8543a5e28d5a3
Dennis Gokman <dennis.gokman@gmail.com>
2013-10-25 21:50:53 -0400

    Sweatr App

---
+++ views/v_users_login.php
@@ -0,0 +1,20 @@
+<form method='POST' action='/users/p_login'>
+
+    Email<br>
+    <input type='text' name='email'>
+
+    <br><br>
+
+    Password<br>
+    <input type='password' name='password'>
+
+    <br><br>
+    <?php if(isset($error)): ?>
+        <p>
+            Login failed. Please double check your email and password.
+        </p>
+        <br>
+    <?php endif; ?>
+    <input type='submit' value='Log in'>
+
+</form>
