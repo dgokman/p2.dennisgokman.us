4fd51c14fcd4060430c9d98e64543652570f2ce0
Dennis Gokman <dennis.gokman@gmail.com>
2013-11-01 02:52:13 -0400

    image uploader 2

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -9,6 +9,8 @@ enctype="multipart/form-data">
 <input type="file" name="file" id="file"><br>
 <input type="submit" name="submit" value="Submit">
 </form>
+
+<img src="/uploads/<?=$_FILES["file"]["name"]?>"></img>
 </body>
 </html>
 
