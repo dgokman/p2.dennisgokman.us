48ecbacf9aeca76f580ace1020b732f98f80cb63
Dennis Gokman <dennis.gokman@gmail.com>
2013-11-01 19:25:16 -0400

    image uploader 2

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -133,22 +133,22 @@ class users_controller extends base_controller {
 
    public function p_upload() {
    $allowedExts = array("gif", "jpeg", "jpg", "png");
-$temp = explode(".", $_FILES["file"]["name"]);
-$extension = end($temp);
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/jpg")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "image/x-png")
-|| ($_FILES["file"]["type"] == "image/png"))
-&& ($_FILES["file"]["size"] < 20000)
-&& in_array($extension, $allowedExts))
-  {
-  if ($_FILES["file"]["error"] > 0)
+   $temp = explode(".", $_FILES["file"]["name"]);
+   $extension = end($temp);
+   if ((($_FILES["file"]["type"] == "image/gif")
+   || ($_FILES["file"]["type"] == "image/jpeg")
+   || ($_FILES["file"]["type"] == "image/jpg")
+   || ($_FILES["file"]["type"] == "image/pjpeg")
+   || ($_FILES["file"]["type"] == "image/x-png")
+   || ($_FILES["file"]["type"] == "image/png"))
+   && ($_FILES["file"]["size"] < 20000)
+   && in_array($extension, $allowedExts))
+    {
+   if ($_FILES["file"]["error"] > 0)
     {
     echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
     }
-  else
+    else
     {
     echo "Upload: " . $_FILES["file"]["name"] . "<br>";
     echo "Type: " . $_FILES["file"]["type"] . "<br>";
@@ -164,6 +164,7 @@ if ((($_FILES["file"]["type"] == "image/gif")
       move_uploaded_file($_FILES["file"]["tmp_name"],
       "uploads/" . $_FILES["file"]["name"]);
       echo "Stored in: " . "uploads/" . $_FILES["file"]["name"];
+     
       }
     }
   }
@@ -171,6 +172,7 @@ else
   {
   echo "Invalid file";
   }
+  
 
 }
 
