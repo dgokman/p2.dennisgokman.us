93ee09dbf37fa08a1cb83b5f20067ebac05978c5
Dennis Gokman <dennis.gokman@gmail.com>
2013-11-29 12:02:32 -0500

    ajax

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -18,8 +18,8 @@ class posts_controller extends base_controller {
         
         # Load JS files
         $client_files_body = Array(
-        "../js/jquery.form.js",
-        "../js/posts_add.js"
+        "/js/jquery.form.js",
+        "/js/posts_add.js"
         );
 
         $this->template->client_files_body = Utils::load_client_files($client_files_body);   
