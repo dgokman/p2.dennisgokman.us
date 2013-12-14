f7f016f634fd46e2b69706a296174f95c54468bd
Dennis Gokman <dennis.gokman@gmail.com>
2013-12-14 00:57:54 -0500

    ajax

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" type="text/css" href="../css/sample-app.css" />
-
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <form method='POST' action='/posts/p_add'>
 
     <label for='content'>New Post:</label><br>
